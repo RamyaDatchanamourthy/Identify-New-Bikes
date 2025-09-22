a60e969a528805cbd971055efc03c7142b138eee
RamyaDatchanamourthy <datrog1621@gmail.com>
2025-09-22 10:10:38 +0530

    Visitng the Home Page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 import IdentifyNewBikes from '../support/pageobjects/IdentifyNewBikes';
-
+ 
 describe("Identify New Bikes", () => {
+  let formdata;
   beforeEach(() => {
     IdentifyNewBikes.visitHomePage();
     Cypress.on('uncaught:exception', (err, runnable) => {
@@ -8,46 +9,4 @@ describe("Identify New Bikes", () => {
     });
   });
 
-  it("should display upcoming Honda bikes under ₹4 lakh", () => {
-    cy.wait(1000)
-    IdentifyNewBikes.clickUpcomingBikesTab();
-    IdentifyNewBikes.getUpcomingBikeElements().each(($el) => {
-      const price = parseFloat($el.attr("data-price"));
-      if (price < 400000) {
-        const bikeName = $el.find(".txt-ulne").text().trim();
-        const launchDate = $el.find(".clr-try").text().trim();
-        cy.log("Bike Name - " + bikeName);
-        cy.log("Price - " + price);
-        cy.log(launchDate);
-        cy.wait(1000)
-      }
-    });
-  });
-
-  it("should extract popular used car models in Chennai", () => {
-    IdentifyNewBikes.visitHomePage()
-    IdentifyNewBikes.clickUsedCarsChennai();
-    cy.wait(1000)
-    let models = [];
-    IdentifyNewBikes.getPopularUsedCarModels().each(($el) => {
-      models.push($el.text().trim());
-    }).then(() => {
-      models.forEach((model) => {
-        cy.log(model);
-      });
-    });
-  });
-
-  it("should simulate Google login and capture error message", () => {
-    IdentifyNewBikes.openLoginModal();
-    IdentifyNewBikes.clickGoogleSignIn();
-
-  });
-});
-
-
-
-
-
-
-
+})
--- cypress/support/pageobjects/IdentifyNewBikes.js
+++ cypress/support/pageobjects/IdentifyNewBikes.js
@@ -2,35 +2,6 @@ class IdentifyNewBikes {
     visitHomePage() {
         cy.visit("https://www.zigwheels.com/");
     }
-
-    clickUpcomingBikesTab() {
-        cy.wait(5000)
-        cy.get(".upcoming-bike-tab").click();
-        cy.wait(1000)
-        cy.get("[title='All Upcoming Bikes']").click();
-        cy.wait(1000)
-        cy.get("[title='upcoming Honda bikes']").click();
-    }
-
-    getUpcomingBikeElements() {
-        return cy.get("#modelList").find("li");
-    }
-
-    clickUsedCarsChennai() {
-        cy.get("#usedCars").find(".Chennai").click({ force: true });
-    }
-
-    getPopularUsedCarModels() {
-        return cy.get(".popularModels").find("li");
-    }
-
-    openLoginModal() {
-        cy.get("#forum_login_cover_image_sm").click({ force: true });
-    }
-
-    clickGoogleSignIn() {
-        cy.get(".googleSignIn").click();
-    }
+    
 }
-
 export default new IdentifyNewBikes();
